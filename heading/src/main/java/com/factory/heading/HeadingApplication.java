0d78a2c22979b09b5aa1d4976f0ac3f9e41f8595
khann <khann@icsp.vn>
2024-10-06 19:30:33 +0700

    init project

---
+++ heading/src/main/java/com/factory/heading/HeadingApplication.java
@@ -0,0 +1,13 @@
+package com.factory.heading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadingApplication.class, args);
+	}
+
+}
---
+++ heading/src/test/java/com/factory/heading/HeadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.factory.heading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
